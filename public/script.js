d624fc4ed235217bfa2225d5dd599666aab4fded
System <none>
2017-09-08 17:20:27 +0000

    👨🙏 Updated with Glitch

--- public/script.js
+++ public/script.js
@@ -5,10 +5,3 @@ Date:
 Purpose:  
 */
 
-function convert() {
-  var degF = document.getElementById("fValue").value
-  var degC = (degF - 32) * (5 / 9);
-  document.getElementById("cValue").innerHTML = degC.toFixed(3);
-}
-
-document.getElementById("button").addEventListener("click", convert, false);
